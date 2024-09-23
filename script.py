9627e03ba91761955bb37decd9b45570f31594b4
korey-byte <korey.sansone@yahoo.com>
2024-09-22 22:49:08 -0500

    Update script.py
    adding the python3 scripts from my readme file

--- script.py
+++ script.py
@@ -1,2 +1,32 @@
-python3-m pip install requests
+# Python3 Commands
+
+Check Python Version: 
+python3 --version
+
+Install a Package: 
+python3 -m pip install <package_name>
+
+Install from requirements.txt: 
+python3 -m pip install -r requirements.txt
+
+Run a Python Script: 
+python3 script_name.py
+
+Create a Virtual Environment: 
+python3 -m venv env
+
+Activate Virtual Environment: 
+source env/bin/activate
+
+Deactivate Virtual Environment: 
+deactivate
+
+Upgrade pip: 
+python3 -m pip install --upgrade pip
+
+List Installed Packages: 
 python3 -m pip list
+
+Uninstall a Package: 
+python3 -m pip uninstall <package_name>
+
